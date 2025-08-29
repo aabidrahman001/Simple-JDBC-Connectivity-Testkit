f8391cffb952687ca8435b702458c4f40d3330d3
Aabid Rahman <aabidrahman001@gmail.com>
2025-08-29 23:15:36 +0600

    code cleaned

--- connect.sh
+++ connect.sh
@@ -1,20 +1,28 @@
-echo "Simple JDBC Connectivity Testkit"
-echo "Select Java version:"
+cat <<'EOF'
+ __ _                 _             __    ___  ___    ___     _____          _   _    _ _   
+/ _(_)_ __ ___  _ __ | | ___        \ \  /   \/ __\  / __\   /__   \___  ___| |_| | _(_) |_ 
+\ \| | '_ ` _ \| '_ \| |/ _ \_____   \ \/ /\ /__\// / /  _____ / /\/ _ \/ __| __| |/ / | __|
+_\ \ | | | | | | |_) | |  __/_____/\_/ / /_// \/  \/ /__|_____/ / |  __/\__ \ |_|   <| | |_ 
+\__/_|_| |_| |_| .__/|_|\___|     \___/___,'\_____/\____/     \/   \___||___/\__|_|\_\_|\__|
+               |_|
+EOF
+echo "Simple-JDBC-Connectivity-Testkit by Aabid"
+echo "Select Java version(1-3):"
 select JAVA_VERSION in "8" "17" "21"; do
     case $JAVA_VERSION in
         8 ) DRIVER="lib/mssql-jdbc-12.8.1.jre8.jar"; HIKARI="lib/HikariCP-4.0.3.jar"; break;;
         17 ) DRIVER="lib/mssql-jdbc-12.8.1.jre11.jar"; HIKARI="lib/HikariCP-5.1.0.jar"; break;;
         21 ) DRIVER="lib/mssql-jdbc-12.8.1.jre11.jar"; HIKARI="lib/HikariCP-5.1.0.jar"; break;;
-        * ) echo "Invalid option";;
+        * ) echo "Invalid option. Input Range must be 1-3";;
     esac
 done
 
-echo "Select Database type:"
+echo "Select Database type(1-2):"
 select DB_TYPE in "mssql" "postgres"; do
     case $DB_TYPE in
         mssql ) DB_DRIVER=$DRIVER; DB_CLASS="com.microsoft.sqlserver.jdbc.SQLServerDriver"; break;;
         postgres ) DB_DRIVER="lib/postgresql-42.7.3.jar"; DB_CLASS="org.postgresql.Driver"; break;;
-        * ) echo "Invalid option";;
+        * ) echo "Invalid option. Input Range must be 1-2";;
     esac
 done
 
